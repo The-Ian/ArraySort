6582f8cb21f70927ecddc3d533edd887d599634c
The-Ian <34355919+The-Ian@users.noreply.github.com>
2018-05-02 21:37:50 -0400

    Rough Draft

---
+++ ArraySort/SortingAlgorithm/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SortingAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultLB = new System.Windows.Forms.ListBox();
+            this.getValBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nameInputTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultTimeLabel = new System.Windows.Forms.Label();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // resultLB
+            // 
+            this.resultLB.FormattingEnabled = true;
+            this.resultLB.Location = new System.Drawing.Point(12, 44);
+            this.resultLB.Name = "resultLB";
+            this.resultLB.Size = new System.Drawing.Size(206, 225);
+            this.resultLB.TabIndex = 0;
+            // 
+            // getValBtn
+            // 
+            this.getValBtn.Location = new System.Drawing.Point(78, 288);
+            this.getValBtn.Name = "getValBtn";
+            this.getValBtn.Size = new System.Drawing.Size(75, 23);
+            this.getValBtn.TabIndex = 1;
+            this.getValBtn.Text = "Get Values";
+            this.getValBtn.UseVisualStyleBackColor = true;
+            this.getValBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sortToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(403, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sortToolStripMenuItem
+            // 
+            this.sortToolStripMenuItem.Name = "sortToolStripMenuItem";
+            this.sortToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.sortToolStripMenuItem.Text = "&Sort";
+            this.sortToolStripMenuItem.Click += new System.EventHandler(this.sortToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.exportToolStripMenuItem.Text = "Ex&port";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // nameInputTB
+            // 
+            this.nameInputTB.Location = new System.Drawing.Point(261, 184);
+            this.nameInputTB.Name = "nameInputTB";
+            this.nameInputTB.Size = new System.Drawing.Size(100, 20);
+            this.nameInputTB.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name Input:";
+            // 
+            // resultTimeLabel
+            // 
+            this.resultTimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.resultTimeLabel.Location = new System.Drawing.Point(261, 66);
+            this.resultTimeLabel.Name = "resultTimeLabel";
+            this.resultTimeLabel.Size = new System.Drawing.Size(100, 84);
+            this.resultTimeLabel.TabIndex = 5;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(261, 222);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 6;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Results:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 323);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.resultTimeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameInputTB);
+            this.Controls.Add(this.getValBtn);
+            this.Controls.Add(this.resultLB);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox resultLB;
+        private System.Windows.Forms.Button getValBtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sortToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox nameInputTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.Label resultTimeLabel;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ArraySort/SortingAlgorithm/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SortingAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int Size = 5000;
+        string[] Names = new string[Size];
+        DateTime startTime = DateTime.Now;
+        DateTime End;
+        TimeSpan Time;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int index = 0;
+                StreamReader inputFile;
+                inputFile = File.OpenText("Names.txt");
+                while (index < Names.Length && !inputFile.EndOfStream)
+                {
+                    Names[index] = inputFile.ReadLine();
+                    index++;
+                }
+                inputFile.Close();
+                foreach (string value in Names)
+                {
+                    resultLB.Items.Add(value);
+                }
+
+                End = DateTime.Now;
+                Time = End - startTime;
+                resultTimeLabel.Text = resultLB.Items.Count.ToString() + " Names added." +
+                    " They were loaded in " + Time.Milliseconds + " milliseconds.";
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string txtPath2 = "SortedNames.txt";
+                StreamWriter sWriter = new StreamWriter(txtPath2);
+                foreach (string name in resultLB.Items)
+
+                {
+
+                    sWriter.WriteLine(name.ToString());
+
+                }
+
+                resultTimeLabel.Text = "File has been exported to a Text file in " + Time.Milliseconds + " milliseconds.";
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("You have already exported the file.");
+            }
+
+        }
+
+        private void sortToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int minIndex;
+            string minVal;
+
+            try
+            {
+                for (int Scan = 0; Scan < Names.Length - 1; Scan++)
+                {
+                    minIndex = Scan;
+                    minVal = Names[Scan];
+
+                    for (int index = Scan + 1; index < Names.Length; index++)
+                    {
+                        if (string.Compare(minVal, Names[index], true) == 1)
+                        {
+                            minVal = Names[index];
+                            minIndex = index;
+                        }
+                    }
+                    Switch(ref Names[minIndex], ref Names[Scan]);
+                }
+
+                resultLB.Items.Clear();
+                foreach (string name in Names)
+                {
+                    resultLB.Items.Add(name);
+                }
+
+                resultTimeLabel.Text = resultLB.Items.Count.ToString() + " Names sorted in alphabetical order." +
+                     " They were loaded in " + Time.Seconds + " seconds.";
+
+            }
+            catch (Exception ex)
+            {
+                 MessageBox.Show("You need to retrieve the values first."); 
+            }
+
+        }
+        private void Switch(ref string one, ref string two)
+        {
+            string tempVal = one;
+            one = two;
+            two = tempVal;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+
